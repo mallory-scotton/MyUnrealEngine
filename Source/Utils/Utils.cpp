4a5ea65b0e35de512bb7847036db4117e3c038fc
mallory-scotton <mallory.scotton@epitech.eu>
2025-03-07 13:52:39 +0100

    add: adding Utils implementation

---
+++ Source/Utils/Utils.cpp
@@ -0,0 +1,65 @@
+///////////////////////////////////////////////////////////////////////////////
+// Dependencies
+///////////////////////////////////////////////////////////////////////////////
+#include "Utils/Utils.hpp"
+#include <cctype>
+
+///////////////////////////////////////////////////////////////////////////////
+// Namespace TKD
+///////////////////////////////////////////////////////////////////////////////
+namespace TKD
+{
+
+///////////////////////////////////////////////////////////////////////////////
+FString Trim(const FString& str)
+{
+    FString result = str;
+
+    result.erase(
+        result.begin(),
+        std::find_if_not(result.begin(), result.end(), ::isspace)
+    );
+
+    result.erase(
+        std::find_if_not(result.rbegin(), result.rend(), ::isspace).base(),
+        result.end()
+    );
+
+    return (result);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+FString ToLower(const FString& str)
+{
+    FString result = str;
+
+    std::transform(result.begin(), result.end(), result.begin(), ::tolower);
+
+    return (result);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+FString ToUpper(const FString& str)
+{
+    FString result = str;
+
+    std::transform(result.begin(), result.end(), result.begin(), ::toupper);
+
+    return (result);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+TVector<FString> Tokenize(const FString& str)
+{
+    TVector<FString> tokens;
+    FStringStream iss(str);
+    FString token;
+
+    while (iss >> token) {
+        tokens.push_back(token);
+    }
+
+    return (tokens);
+}
+
+} // !namespace TKD
