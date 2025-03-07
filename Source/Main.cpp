98ee1ca5811c44f92a50e3bfecf0f04026ed0ab1
mallory-scotton <mallory.scotton@epitech.eu>
2025-03-07 13:51:46 +0100

    add: adding tests main

---
+++ Source/Main.cpp
@@ -0,0 +1,74 @@
+///////////////////////////////////////////////////////////////////////////////
+// Dependencies
+///////////////////////////////////////////////////////////////////////////////
+#include "Utils/Types.hpp"
+#include "GraphEditor/UClass.hpp"
+#include "GraphEditor/Nodes/ArithmeticNodes.hpp"
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <imgui.h>
+#include <imgui_internal.h>
+#include <imgui-SFML.h>
+#include <imgui_node_editor.h>
+#include <widgets.h>
+
+///////////////////////////////////////////////////////////////////////////////
+namespace en = ax::NodeEditor;
+
+///////////////////////////////////////////////////////////////////////////////
+int main(void)
+{
+    sf::RenderWindow window(sf::VideoMode({1200, 800}), "GameEngine");
+    sf::Clock clock;
+
+    if (!ImGui::SFML::Init(window)) {
+        std::cerr << "Coudl'nt initialize ImGui" << std::endl;
+        return (EXIT_FAILURE);
+    }
+
+    ImGuiWindowFlags wflags =
+        ImGuiWindowFlags_NoBringToFrontOnFocus |
+        ImGuiWindowFlags_NoTitleBar |
+        ImGuiWindowFlags_NoResize |
+        ImGuiWindowFlags_NoScrollbar |
+        ImGuiWindowFlags_NoScrollWithMouse;
+
+    en::EditorContext* context = en::CreateEditor();
+
+    TKD::UClass myClass("MyClass");
+
+    while (window.isOpen()) {
+        while (auto event = window.pollEvent()) {
+            ImGui::SFML::ProcessEvent(window, *event);
+
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+
+        ImGui::SFML::Update(window, clock.restart());
+
+        en::SetCurrentEditor(context);
+
+        ImGui::SetNextWindowPos(ImVec2(0, 0));
+        ImGui::SetNextWindowSize(ImGui::GetIO().DisplaySize);
+
+        ImGui::Begin("Main", nullptr, wflags);
+
+        myClass.GetGraph()->Render();
+
+        ImGui::End();
+
+        en::SetCurrentEditor(nullptr);
+
+        window.clear();
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    en::DestroyEditor(context);
+    ImGui::SFML::Shutdown();
+
+    return (0);
+}
