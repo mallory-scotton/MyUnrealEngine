020ae02eafcf5ec95a75a2028709e8694780ab7e
mallory-scotton <mallory.scotton@epitech.eu>
2025-03-10 15:39:34 +0100

    remove: removing main

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -42,22 +42,12 @@ int main(void)
 
     TKD::UClass myClass("MyClass");
 
-    TKD::USaveArchive archive("graph.uassets");
-
-    bool s = false;
-
     while (window.isOpen()) {
         while (auto event = window.pollEvent()) {
             ImGui::SFML::ProcessEvent(window, *event);
 
             if (event->is<sf::Event::Closed>()) {
                 window.close();
-            }
-
-            if (auto key = event->getIf<sf::Event::KeyReleased>()) {
-                if (key->code == sf::Keyboard::Key::S) {
-                    s = true;
-                }
             }
         }
 
@@ -73,11 +63,6 @@ int main(void)
         myClass.GetGraph()->Render();
 
         ImGui::End();
-
-        if (s) {
-            myClass.GetGraph()->Serialize(archive);
-            s = false;
-        }
 
         en::SetCurrentEditor(nullptr);
 
