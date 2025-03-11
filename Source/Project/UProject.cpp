517232cd60801d7dcb2549e4ce2b4290678fd24f
mallory-scotton <mallory.scotton@epitech.eu>
2025-03-11 01:27:01 +0100

    add: adding project creation

--- Source/Project/UProject.cpp
+++ Source/Project/UProject.cpp
@@ -2,6 +2,10 @@
 // Dependencies
 ///////////////////////////////////////////////////////////////////////////////
 #include "Project/UProject.hpp"
+#include "Utils/Filesystem.hpp"
+#include "Utils/Archive.hpp"
+#include "Utils/SaveArchive.hpp"
+#include "Config.hpp"
 
 ///////////////////////////////////////////////////////////////////////////////
 // Namespace TKD
@@ -9,6 +13,47 @@
 namespace TKD
 {
 
+///////////////////////////////////////////////////////////////////////////////
+UProject::UProject(void)
+{}
 
+///////////////////////////////////////////////////////////////////////////////
+UProject::~UProject()
+{}
+
+///////////////////////////////////////////////////////////////////////////////
+TSharedPtr<UProject> UProject::CreateProject(
+    const FString& name,
+    const FPath& location
+)
+{
+    FPath root = location / name;
+    FPath content = root / "Content";
+
+    try {
+        Fs::Mkdir(root);
+        Fs::Mkdir(content);
+
+        USaveArchive project(root / (name + ".uproject"));
+
+        FUint8 major = (FUint8)UEB_VERSION_MAJOR;
+        FUint8 minor = (FUint8)UEB_VERSION_MINOR;
+        FUint8 patch = (FUint8)UEB_VERSION_PATCH;
+        FString projectName = name;
+
+        project << major << minor << patch << projectName;
+
+    } catch (const std::exception& e) {}
+    return (LoadProject(location));
+}
+
+///////////////////////////////////////////////////////////////////////////////
+TSharedPtr<UProject> UProject::LoadProject(const FPath& location)
+{
+    if (!Fs::FileExists(location))
+        return (nullptr);
+    // TODO: Add project loading
+    return (nullptr);
+}
 
 } // !namespace TKD
--- Source/Project/UProject.hpp
+++ Source/Project/UProject.hpp
@@ -19,6 +19,32 @@ namespace TKD
 ///
 ///////////////////////////////////////////////////////////////////////////////
 class UProject
-{};
+{
+public:
+    UProject(void);
+    ~UProject();
+
+private:
+    FString mName;
+    FPath mLocation;
+    // TODO: Add settings
+    // TODO: Add content browser (TUniquePtr)
+    bool mInitialized;
+    bool mIsDirty;
+
+public:
+    static TSharedPtr<UProject> CreateProject(const FString& name, const FPath& location);
+    static TSharedPtr<UProject> LoadProject(const FPath& location);
+
+public:
+    bool Initialize(void);
+    bool Save(void);
+    bool Close(void);
+    bool HasUnsavedChanges(void) const;
+    // TODO: Add settings
+    // TODO: Add content browser *
+    FPath GetProjectDirectory(void) const;
+    FPath GetContentDirectory(void) const;
+};
 
 } // !namespace TKD
