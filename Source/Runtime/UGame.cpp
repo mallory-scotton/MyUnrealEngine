e1ad1ad1a7786cbddfe7f8bc6442021b2c77149a
mallory-scotton <mallory.scotton@epitech.eu>
2025-03-12 17:01:31 +0100

    add: adding UGame class

---
+++ Source/Runtime/UGame.cpp
@@ -0,0 +1,79 @@
+///////////////////////////////////////////////////////////////////////////////
+// Dependencies
+///////////////////////////////////////////////////////////////////////////////
+#include "Runtime/UGame.hpp"
+
+///////////////////////////////////////////////////////////////////////////////
+// Namespace UEB
+///////////////////////////////////////////////////////////////////////////////
+namespace UEB
+{
+
+///////////////////////////////////////////////////////////////////////////////
+UGame::UGame(TSharedPtr<UEB::UWorld> world)
+    : mWorld(world)
+    , mClosed(true)
+{}
+
+///////////////////////////////////////////////////////////////////////////////
+UGame::~UGame()
+{
+    Close();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void UGame::Initialize(void)
+{
+    mWindow = std::make_unique<sf::RenderWindow>(
+        sf::VideoMode({800, 600}), "UEB", sf::Style::Close);
+    mClock = std::make_unique<sf::Clock>();
+    mClosed = false;
+    if (mWorld) {
+        mWorld->BeginPlay();
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void UGame::Run(void)
+{
+    if (mClosed || !mWindow || !mWindow->isOpen()) {
+        return;
+    }
+
+    while (const auto& event = mWindow->pollEvent()) {
+        if (event->is<sf::Event::Closed>()) {
+            Close();
+        }
+    }
+
+    mWindow->clear();
+
+    if (mWorld) {
+        mWorld->Tick(mClock->restart().asSeconds());
+    }
+
+    mWindow->display();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+bool UGame::IsClosed(void) const
+{
+    return (mClosed);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+void UGame::Close(void)
+{
+    if (mClosed == true) {
+        return;
+    }
+    if (mWorld) {
+        mWorld->EndPlay();
+    }
+    if (mWindow && mWindow->isOpen()) {
+        mWindow->close();
+    }
+    mClosed = true;
+}
+
+} // !namespace UEB
